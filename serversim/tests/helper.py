fab6ba6f4af9df8152db0000fdee7e396befd957
PVillela <pvillela@gmail.com>
2017-07-30 17:58:14 -0400

    Created class to remove need for module global variables in tests.

--- serversim/tests/helper.py
+++ serversim/tests/helper.py
@@ -8,23 +8,6 @@ from hypothesis.strategies import composite, integers, floats, lists, choices
 from serversim import *
 
 
-########################
-# Module global parameters
-
-_minHwThreads = 1
-_maxHwThreads = 100
-_minSwThreadsFactor = 1
-_maxSwThreadsFactor = 100
-_minSpeed = 1
-_maxSpeed = 100
-
-_minCompUnits = 1
-_maxCompUnits = 100
-
-# end Parameters
-########################
-
-
 @pytest.fixture(scope="module")
 def fi():
     return const.logfile
@@ -43,6 +26,20 @@ def cug(mid, delta):
 
 
 def gen_servers(env, nServers):
+    """ Manual random generation of lists of servers.
+
+    Does not depend on Hypothesis.
+
+    :param env: simpy.Environment.
+    :param nServers: number of servers
+    :return: random list of servers
+    """
+    _minHwThreads = 1
+    _maxHwThreads = 100
+    _minSwThreadsFactor = 1
+    _maxSwThreadsFactor = 100
+    _minSpeed = 1
+    _maxSpeed = 100
 
     def fServerIter():
         for i in range(0, nServers):
@@ -56,6 +53,9 @@ def gen_servers(env, nServers):
 
 
 def gen_core_svc_requesters(env, serverLst, ffServer, svcReqLog, nSvcRqrs):
+
+    _minCompUnits = 1
+    _maxCompUnits = 100
 
     def fSvcRqrIter():
         for i in range(0, nSvcRqrs):
@@ -89,48 +89,87 @@ def dump_servers(serverLst):
         print(indent * 2 + "utilization = %s" % server.utilization)
 
 
-def server_st(env):
-    @composite
-    def servers_strat0(draw, env, seq):
-        hwThreads = draw(integers(_minHwThreads, _maxHwThreads))
-        swThreads = hwThreads * draw(integers(_minSwThreadsFactor,
-                                              _maxSwThreadsFactor))
-        speed = draw(floats(_minSpeed, _maxSpeed))
-        seq[0] += 1
-        return Server(env, hwThreads, swThreads, speed, "Server_" + str(seq[0]))
-
-    seq = {0:0}
-    return servers_strat0(env, seq)
+def defaultFfServer(serverLst):
+    def fServer(_svcReqName):
+        return random.choice(serverLst)
+    return fServer
 
 
-def svc_rqr_st(env, serverLst, ffServer, svcReqLogDict):
-    @composite
-    def svc_rqrs_strat0(draw, env, serverLst, ffServer, svcReqLogDict, seq):
-        compUnits = draw(floats(_minCompUnits, _maxCompUnits))
-        fServer = ffServer(serverLst)
-        seq[0] += 1
-        svcReqLog = svcReqLogDict[0]
-        return CoreSvcRequester(env, "Txn_" + str(seq[0]), cug(compUnits, compUnits/2), fServer, svcReqLog)
+class CustomGenerationStrategies(object):
+    """ Exposes custom Hypothesis generation strategies as methods.
 
-    seq = {0:0}
-    return svc_rqrs_strat0(env, serverLst, ffServer, svcReqLogDict, seq)
+    The strategies are parameterized by the attributes of the object
+    instance.
+    """
+
+    def __init__(self,
+                 minHwThreads=1,
+                 maxHwThreads=100,
+                 minSwThreadsFactor=1,
+                 maxSwThreadsFactor=100,
+                 minSpeed=1,
+                 maxSpeed=100,
+                 minCompUnits=1,
+                 maxCompUnits=100,
+                 ffServer=defaultFfServer,
+                 maxServers=10,
+                 maxSvcRqrs=20,
+                 svcReqLog=None,
+                 testCount=0):
+        self.minHwThreads = minHwThreads
+        self.maxHwThreads = maxHwThreads
+        self.minSwThreadsFactor = minSwThreadsFactor
+        self.maxSwThreadsFactor = maxSwThreadsFactor
+        self.minSpeed = minSpeed
+        self.maxSpeed = maxSpeed
+        self.minCompUnits = minCompUnits
+        self.maxCompUnits = maxCompUnits
+        self.ffServer = ffServer
+        self.maxServers = maxServers
+        self.maxSvcRqrs = maxSvcRqrs
+        if svcReqLog is None:
+            svcReqLog = list()
+        self.svcReqLog = svcReqLog
+        self.testCount = testCount
 
 
-@composite
-def servers_svc_rqrs_st(draw, env, maxServers, maxSvcRqrs, svcReqLogDict):
+    def server(self, env):
+        @composite
+        def servers_strat0(draw, seq):
+            hwThreads = draw(integers(self.minHwThreads, self.maxHwThreads))
+            swThreads = hwThreads * draw(integers(self.minSwThreadsFactor,
+                                                  self.maxSwThreadsFactor))
+            speed = draw(floats(self.minSpeed, self.maxSpeed))
+            seq[0] += 1
+            return Server(env, hwThreads, swThreads, speed,
+                          "Server_" + str(seq[0]))
 
-    svcReqLog = svcReqLogDict[0]
+        seq = {0: 0}
+        return servers_strat0(seq)
 
-    choice = draw(choices())
-    nServers = draw(integers(1, maxServers))
-    nSvcRqrs = draw(integers(1, maxSvcRqrs))
 
-    def ffServer(serverLst):
-        def fServer(_svcReqName):
-            return choice(serverLst)
-        return fServer
+    def svc_rqr(self, env, serverLst):
+        @composite
+        def svc_rqrs_strat0(draw, serverLst, seq):
+            compUnits = draw(floats(self.minCompUnits, self.maxCompUnits))
+            fServer = self.ffServer(serverLst)
+            seq[0] += 1
+            return CoreSvcRequester(env, "Txn_" + str(seq[0]),
+                                    cug(compUnits, compUnits / 2), fServer,
+                                    self.svcReqLog)
 
-    serverLst = draw(lists(server_st(env), nServers, None, nServers))
-    svcRqrLst = draw(lists(svc_rqr_st(env, serverLst, ffServer, {0:svcReqLog}), nSvcRqrs, None, nSvcRqrs))
+        seq = {0: 0}
+        return svc_rqrs_strat0(serverLst, seq)
 
-    return (serverLst, svcRqrLst)
+
+    def servers_svc_rqrs(self, env):
+        @composite
+        def servers_svc_rqrs0(draw):
+            nServers = draw(integers(1, self.maxServers))
+            nSvcRqrs = draw(integers(1, self.maxSvcRqrs))
+            serverLst = draw(lists(self.server(env), nServers, None, nServers))
+            svcRqrLst = draw(
+                lists(self.svc_rqr(env, serverLst), nSvcRqrs, None, nSvcRqrs))
+            return (serverLst, svcRqrLst)
+
+        return servers_svc_rqrs0()
--- serversim/tests/test_core_svc_requester.py
+++ serversim/tests/test_core_svc_requester.py
@@ -12,20 +12,10 @@ from hamcrest import assert_that, close_to #, greater_than, less_than, equal_to
 from hypothesis import given
 from hypothesis.strategies import data, choices, lists, integers
 
-from helper import fi, dump_servers, dump_svc_reqs, servers_svc_rqrs_st
+from helper import fi, dump_servers, dump_svc_reqs, CustomGenerationStrategies
 
 
-####################
-# Module globals
-
-_testCount = 0
-
-# end module globals
-####################
-
-
-@given(data())
-def t_core_svc_requester(fi, maxServers, maxSvcRqrs, maxSvcReqs, dump, data):
+def t_core_svc_requester(fi, maxServers, maxSvcRqrs, maxSvcReqs, dump):
     """
     Scenario: A core service request's process time on a server
         The process time is request.compUnits / (server.speed /
@@ -45,108 +35,112 @@ def t_core_svc_requester(fi, maxServers, maxSvcRqrs, maxSvcReqs, dump, data):
                 time over all core service requests processed on the server.
     """
 
-    global _testCount
+    cgs = CustomGenerationStrategies(
+        maxServers=maxServers,
+        maxSvcRqrs=maxSvcRqrs,
+    )
 
-    draw = data.draw
-    choice = draw(choices())
 
-    env = simpy.Environment()
-    svcReqLog = list()
+    @given(data())
+    def f(data):
 
-    serverLst, svcRqrLst = draw(
-        servers_svc_rqrs_st(env, maxServers, maxSvcRqrs, {0:svcReqLog}))
-    nServers = len(serverLst)
-    nSvcRqrs = len(svcRqrLst)
-    nSvcReqs = draw(integers(1, maxSvcReqs))
+        draw = data.draw
+        env = simpy.Environment()
+        cgs.testCount = cgs.testCount + 1
+        serverLst, svcRqrLst = draw(cgs.servers_svc_rqrs(env))
+        nSvcReqs = draw(integers(1, maxSvcReqs))
+        svcReqLog = cgs.svcReqLog
+        nServers = len(serverLst)
+        nSvcRqrs = len(svcRqrLst)
+    
+        print("\n\n@@@@@@@@ Start test :", cgs.testCount, ", nServers:", nServers,
+              ", nSvcRqrs:", nSvcRqrs, "nSvcReqs:", nSvcReqs, file=fi)
+    
+        for i in range(nSvcReqs):
+            # svcRqr = choice(svcRqrLst)  # this is expensive, replaced below
+            j = random.randint(0, len(svcRqrLst) - 1)
+            svcRqr = svcRqrLst[j]
+            svcReq = svcRqr.makeSvcRequest(None)
+            # print(">>> submitting", svcRqr.svcName, file=fi)
+            svcReq.submit()
+    
+        simTime = 1000000000
+        print("\n\n***** Start Simulation (%s, %s, %s)-%s *****" % (
+            nServers, nSvcRqrs, dump, cgs.testCount), file=fi)
+        print("Simulation: simTime = %s" % simTime, file=fi)
+        env.run(until=simTime)
+    
+        delta = 0.0001
+    
+        if dump: dump_svc_reqs(svcReqLog)
+    
+        # Scenario: A core service request's process time on a server
+        #     The process time is request.compUnits / (server.speed /
+        #         server.maxConcurrency).
+        for svcReq in svcReqLog:
+            server = svcReq.server
+            assert_that(svcReq.processTime,
+                        close_to(svcReq.compUnits / (
+                        server.speed / server.maxConcurrency), delta))
+    
+        if dump: dump_servers(serverLst)
+    
+        # Scenario: Server average time stats
+        #     Server stats are aggregates of service request stats for all core
+        #         service requests that have completed processing on the server:
+        #
+        #         The server's average processing time is the average of the
+        #             processing time over all core service requests processed
+        #             on the server.
+        #         The server's average (hardware thread) queue time is the average
+        #             of the (hardware thread) queue time over all core service
+        #             requests processed on the server.
+        #         The server's average service time is the average of the service
+        #             time over all core service requests processed on the server.
+        for server in serverLst:
+            print(">>>", server.name, "hwThreads=", server.maxConcurrency,
+                  "_threads=", server.numThreads, "speed=", server.speed, file=fi)
+            svcReqLog = server.svcReqLog
+            nSvcReqs = len(svcReqLog)
+            print("nSvcReqs", nSvcReqs, file=fi)
+            if nSvcReqs != 0:
+                print("svcReq processTimes",
+                      [(svcReq.svcName, svcReq.processTime) for svcReq in
+                       svcReqLog], file=fi)
+                print("svcReq hwQueueTimes",
+                      [(svcReq.svcName, svcReq.hwQueueTime) for svcReq in
+                       svcReqLog], file=fi)
+                print("svcReq serviceTimes",
+                      [(svcReq.svcName, svcReq.serviceTime) for svcReq in
+                       svcReqLog], file=fi)
+                avgSvcReqProcessTime = \
+                    sum([svcReq.processTime for svcReq in svcReqLog]) / nSvcReqs
+                avgSvcReqHwQueueTime = \
+                    sum([svcReq.hwQueueTime for svcReq in svcReqLog]) / nSvcReqs
+                avgSvcReqServiceTime = \
+                    sum([svcReq.serviceTime for svcReq in svcReqLog]) / nSvcReqs
+                print("Before assertions", file=fi)
+                print(avgSvcReqProcessTime, server.avgProcessTime, file=fi)
+                print(avgSvcReqHwQueueTime, server.avgHwQueueTime, file=fi)
+                print(avgSvcReqServiceTime, server.avgServiceTime, file=fi)
+                assert_that(avgSvcReqProcessTime,
+                            close_to(server.avgProcessTime, delta))
+                assert_that(avgSvcReqHwQueueTime,
+                            close_to(server.avgHwQueueTime, delta))
+                assert_that(avgSvcReqServiceTime,
+                            close_to(server.avgServiceTime, delta))
+    
+        print("@@@@@@@@ End test: " + str(cgs.testCount) + " ended: " + str(env.now),
+              file=fi)
 
-    _testCount = _testCount + 1
-
-    print("\n\n@@@@@@@@ Start test :", _testCount, ", nServers:", nServers,
-          ", nSvcRqrs:", nSvcRqrs, "nSvcReqs:", nSvcReqs, file=fi)
-
-    for i in range(nSvcReqs):
-        # svcRqr = choice(svcRqrLst)  # this is expensive, replaced below
-        j = random.randint(0, len(svcRqrLst) - 1)
-        svcRqr = svcRqrLst[j]
-        svcReq = svcRqr.makeSvcRequest(None)
-        # print(">>> submitting", svcRqr.svcName, file=fi)
-        svcReq.submit()
-
-    simTime = 1000000000
-    print("\n\n***** Start Simulation (%s, %s, %s)-%s *****" % (
-        nServers, nSvcRqrs, dump, _testCount), file=fi)
-    print("Simulation: simTime = %s" % simTime, file=fi)
-    env.run(until=simTime)
-
-    delta = 0.0001
-
-    if dump: dump_svc_reqs(svcReqLog)
-
-    # Scenario: A core service request's process time on a server
-    #     The process time is request.compUnits / (server.speed /
-    #         server.maxConcurrency).
-    for svcReq in svcReqLog:
-        server = svcReq.server
-        assert_that(svcReq.processTime,
-                    close_to(svcReq.compUnits / (
-                    server.speed / server.maxConcurrency), delta))
-
-    if dump: dump_servers(serverLst)
-
-    # Scenario: Server average time stats
-    #     Server stats are aggregates of service request stats for all core
-    #         service requests that have completed processing on the server:
-    #
-    #         The server's average processing time is the average of the
-    #             processing time over all core service requests processed
-    #             on the server.
-    #         The server's average (hardware thread) queue time is the average
-    #             of the (hardware thread) queue time over all core service
-    #             requests processed on the server.
-    #         The server's average service time is the average of the service
-    #             time over all core service requests processed on the server.
-    for server in serverLst:
-        print(">>>", server.name, "hwThreads=", server.maxConcurrency,
-              "_threads=", server.numThreads, "speed=", server.speed, file=fi)
-        svcReqLog = server.svcReqLog
-        nSvcReqs = len(svcReqLog)
-        print("nSvcReqs", nSvcReqs, file=fi)
-        if nSvcReqs != 0:
-            print("svcReq processTimes",
-                  [(svcReq.svcName, svcReq.processTime) for svcReq in
-                   svcReqLog], file=fi)
-            print("svcReq hwQueueTimes",
-                  [(svcReq.svcName, svcReq.hwQueueTime) for svcReq in
-                   svcReqLog], file=fi)
-            print("svcReq serviceTimes",
-                  [(svcReq.svcName, svcReq.serviceTime) for svcReq in
-                   svcReqLog], file=fi)
-            avgSvcReqProcessTime = \
-                sum([svcReq.processTime for svcReq in svcReqLog]) / nSvcReqs
-            avgSvcReqHwQueueTime = \
-                sum([svcReq.hwQueueTime for svcReq in svcReqLog]) / nSvcReqs
-            avgSvcReqServiceTime = \
-                sum([svcReq.serviceTime for svcReq in svcReqLog]) / nSvcReqs
-            print("Before assertions", file=fi)
-            print(avgSvcReqProcessTime, server.avgProcessTime, file=fi)
-            print(avgSvcReqHwQueueTime, server.avgHwQueueTime, file=fi)
-            print(avgSvcReqServiceTime, server.avgServiceTime, file=fi)
-            assert_that(avgSvcReqProcessTime,
-                        close_to(server.avgProcessTime, delta))
-            assert_that(avgSvcReqHwQueueTime,
-                        close_to(server.avgHwQueueTime, delta))
-            assert_that(avgSvcReqServiceTime,
-                        close_to(server.avgServiceTime, delta))
-
-    print("@@@@@@@@ End test: " + str(_testCount) + " ended: " + str(env.now),
-          file=fi)
+    return f()
 
 
 @pytest.mark.parametrize(
     "maxServers, maxSvcRqrs, maxSvcReqs, dump", [
         # (1, 1, 100, False),
         # (5, 20, 100, True),
-        (10, 20, 2000, False)
+        (10, 20, 1000, False)
     ]
 )
 def test_core_svc_requester1(fi, maxServers, maxSvcRqrs, maxSvcReqs, dump):
