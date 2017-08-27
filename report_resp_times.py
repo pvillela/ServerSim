70695a6fe76d9b3505852504c5ab5aded89abcc5
PVillela <pvillela@gmail.com>
2017-08-27 11:49:40 -0400

    Small refactoring of minibatching and plotting.

--- report_resp_times.py
+++ report_resp_times.py
@@ -1,5 +1,5 @@
-from typing import TYPE_CHECKING, Sequence, Tuple
-from collections import OrderedDict
+from typing import TYPE_CHECKING
+from collections import OrderedDict, namedtuple
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -8,8 +8,11 @@ if TYPE_CHECKING:
     from serversim import UserGroup
 
 
+Minibatch = namedtuple("Result", ["ts", "counts", "means", "q_95"])
+
+
 def minibatch_resp_times(time_resolution, grp):
-    # type: (float, UserGroup) -> Tuple[Sequence[float], Sequence[float], Sequence[float], Sequence[float], Sequence[float], Sequence[float]]
+    # type: (float, UserGroup) -> Minibatch
 
     times = ((svc_req.time_dict["submitted"] // time_resolution) *
              time_resolution
@@ -27,50 +30,44 @@ def minibatch_resp_times(time_resolution, grp):
     ts = counts_ser.index.values
     counts = counts_ser.values
     means = grouped.mean().values
-    q_50 = grouped.quantile(.50).values
     q_95 = grouped.quantile(.95).values
-    q_99 = grouped.quantile(.99).values
 
-    return ts, counts, means, q_50, q_95, q_99
+    return Minibatch(ts, counts, means, q_95)
 
 
-def plot_counts_means_q95(quantiles1, quantiles2):
+def plot_counts_means_q95(minibatch1, minibatch2):
+    # type: (Minibatch, Minibatch) -> None
 
-    x = quantiles1[0]  # should be same as quantiles2[0]
+    x = minibatch1.ts  # should be same as minibatch2.ts
 
-    counts1 = quantiles1[1]
-    counts2 = quantiles2[1]
+    counts1 = minibatch1.counts
+    counts2 = minibatch2.counts
 
-    means1 = quantiles1[2]
-    means2 = quantiles2[2]
+    means1 = minibatch1.means
+    means2 = minibatch2.means
 
-    q1_95 = quantiles1[4]
-    q2_95 = quantiles2[4]
+    q1_95 = minibatch1.q_95
+    q2_95 = minibatch2.q_95
+
+    fig = plt.figure()
 
     # Plot counts
-    plt.plot(x, counts1, color='b', label="Counts 1")
-    plt.plot(x, counts2, color='r', label="Counts 2")
-    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    plt.xlabel("Time buckets")
-    plt.ylabel("Throughput")
-    plt.show()
+    axc = fig.add_axes([0, .97, .77, .77])
+    axc.plot(x, counts1, color='b', label="Counts 1")
+    axc.plot(x, counts2, color='r', label="Counts 2")
+    axc.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    axc.set_xlabel("Time buckets")
+    axc.set_ylabel("Throughput")
 
-    # Plot averages and 95th percentiles
-
-    plt.plot(x, means1, color='b', label="Means 1")
-    plt.plot(x, q1_95, color='c', label="95th Percentile 1")
-
-    plt.plot(x, means2, color='r', label="Means 2")
-    plt.plot(x, q2_95, color='m', label="95th Percentile 2")
-
-    # Hack to avoid duplicated labels (https://stackoverflow.com/questions/13588920/stop-matplotlib-repeating-labels-in-legend)
-    handles, labels = plt.gca().get_legend_handles_labels()
-    by_label = OrderedDict(zip(labels, handles))
-    plt.legend(by_label.values(), by_label.keys(), bbox_to_anchor=(1.05, 1),
-               loc=2, borderaxespad=0.)
-
-    plt.xlabel("Time buckets")
-    plt.ylabel("Response times")
+    # Plot response time averages and 95th percentiles
+    axr = fig.add_axes([0, 0, .77, .77])
+    axr.plot(x, means1, color='b', label="Means 1")
+    axr.plot(x, q1_95, color='c', label="95th Percentile 1")
+    axr.plot(x, means2, color='r', label="Means 2")
+    axr.plot(x, q2_95, color='m', label="95th Percentile 2")
+    axr.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    axr.set_xlabel("Time buckets")
+    axr.set_ylabel("Response times")
 
     plt.show()
 
@@ -79,7 +76,7 @@ def compare_scenarios(sc1, sc2):
     grp1 = sc1.grp
     grp2 = sc2.grp
 
-    quantiles1 = minibatch_resp_times(5, grp1)
-    quantiles2 = minibatch_resp_times(5, grp2)
+    minibatch1 = minibatch_resp_times(5, grp1)
+    minibatch2 = minibatch_resp_times(5, grp2)
 
-    plot_counts_means_q95(quantiles1, quantiles2)
+    plot_counts_means_q95(minibatch1, minibatch2)
